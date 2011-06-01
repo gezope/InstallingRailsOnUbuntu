ca5310ad68b692e0a3d44e6ab394e7e7fc27896c
gezope <gezope@gmail.com>
2011-05-31 18:21:15 -0700

    autotest gems added

--- step7.rb
+++ step7.rb
@@ -1,6 +1,8 @@
 # Step 7: Install Rails and other gems
 
-$ gem install rails haml compass aws-ses aws-s3 authlogic cucumber datamapper json mongo mongoid oauth authlogic-oauth padrino omniauth pdf-reader pg rack rack-cache sinatra sqlite3 thin spork rspec rspec-rails facroty_girl_rails nokogiri annotate_models faker will_paginate
+$ gem install rails haml compass aws-ses aws-s3 authlogic cucumber datamapper json mongo mongoid oauth authlogic-oauth padrino omniauth pdf-reader pg rack rack-cache sinatra sqlite3 thin spork rspec rspec-rails facroty_girl_rails nokogiri annotate_models faker will_paginate 
+$gem install autotest -v 4.4.6
+$gem install autotest-rails-pure -v 4.1.2
 
 # Important: check if everything works fine
 $ rvm -v
