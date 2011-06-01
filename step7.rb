53f68e1b47a599ab660c6fed5eaa96caae90abdb
gezope <gezope@gmail.com>
2011-05-31 17:50:19 -0700

    added more gems to install

--- step7.rb
+++ step7.rb
@@ -1,6 +1,6 @@
 # Step 7: Install Rails and other gems
 
-$ gem install rails haml compass aws-ses aws-s3 authlogic cucumber datamapper json mongo mongoid oauth authlogic-oauth padrino omniauth pdf-reader pg rack rack-cache sinatra sqlite3 thin
+$ gem install rails haml compass aws-ses aws-s3 authlogic cucumber datamapper json mongo mongoid oauth authlogic-oauth padrino omniauth pdf-reader pg rack rack-cache sinatra sqlite3 thin spork rspec rspec-rails facroty_girl_rails nokogiri annotate_models faker will_paginate
 
 # Important: check if everything works fine
 $ rvm -v
