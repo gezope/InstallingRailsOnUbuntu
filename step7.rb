1555bfef8bca6cc24974313a65e5da8b62efbf1f
gezope <gezope@gmail.com>
2011-05-31 18:22:10 -0700

    more autotest

--- step7.rb
+++ step7.rb
@@ -1,8 +1,10 @@
 # Step 7: Install Rails and other gems
 
 $ gem install rails haml compass aws-ses aws-s3 authlogic cucumber datamapper json mongo mongoid oauth authlogic-oauth padrino omniauth pdf-reader pg rack rack-cache sinatra sqlite3 thin spork rspec rspec-rails facroty_girl_rails nokogiri annotate_models faker will_paginate 
-$gem install autotest -v 4.4.6
-$gem install autotest-rails-pure -v 4.1.2
+$ gem install autotest -v 4.4.6
+$ gem install autotest-rails-pure -v 4.1.2
+$ gem install autotest-fsevent -v 0.2.4
+$ gem install autotest-growl -v 0.2.9
 
 # Important: check if everything works fine
 $ rvm -v
