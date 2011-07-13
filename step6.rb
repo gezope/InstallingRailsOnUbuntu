205796860abbe288377adee61c9081e5ac35d1a8
Zoltan Gero <gezope@gmail.com>
2011-07-13 23:23:17 +0200

    step 6 about Gemsets created

---
+++ step6.rb
@@ -0,0 +1,8 @@
+#Create Gemsets to avoid confusions with gems versions and updates.
+# More info about Gemsets: 
+# http://beginrescueend.com/gemsets/basics/
+
+$ rvm gemset create YourProjectName
+$ rvm 1.9.2@YourProjectName
+# important format: rvm then your Ruby version from rvm list and then your Gemset's name
+'
