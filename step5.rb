ffc5bc27516e6e17609796841b2dcf42a05585f2
gezope <gezope@gmail.com>
2011-07-13 03:41:00 -0700

    Set default Ruby

--- step5.rb
+++ step5.rb
@@ -1,3 +1,16 @@
 # Step 5:
 
+# Install Ruby version 1.9.2 with RVM.
 $ rvm install 1.9.2 --with-readline-dir=$rvm_path/usr --with-iconv-dir=$rvm_path/usr --with-zlib-dir=$rvm_path/usr --with-openssl-dir=/usr/local 
+
+# Set the default Ruby to use.
+$ rvm use 1.9.2
+
+# Check if works:
+$ ruby -v
+$ rvm list
+
+# Set the default Ruby interpreter:
+$ rvm use 1.9.2 --default
+
+# It overrides the system's default Ruby. Also next time you don't need to specify which Ruby version you want.
